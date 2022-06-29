a09f8c0a35be52029f7dd220bf1726409eb63849
Mike Crawford <dev.random.87@gmail.com>
2022-06-29 12:59:09 -0700

    Add variable declarations

--- Foundations/javascript-1/javascript.js
+++ Foundations/javascript-1/javascript.js
@@ -1,1 +1,18 @@
 console.log("Hello again, world!")
+console.log("Now let's look at some variables!")
+
+// Declare a variable
+let message;
+// Store the string in the variable message
+message = 'Hello for the third time!'
+// Popup alert printing the message
+// alert(message)
+// Print message to the console
+console.log(message)
+
+
+// More concise version
+let message2 = 'Hello for the fourth time!'
+console.log(message2)
+
+
