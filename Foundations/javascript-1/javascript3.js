65aecbbf963fcebeb7cee79db20a86c16c1674e5
Mike Crawford <dev.random.87@gmail.com>
2022-06-30 07:01:21 -0700

    Add age functionality

--- Foundations/javascript-1/javascript3.js
+++ Foundations/javascript-1/javascript3.js
@@ -2,7 +2,10 @@ const button = document.querySelector('button');
 
 function greet() {
     const name = prompt('What is your name? ');
-    alert(`Hello, ${name}!`);
+    const birthYear = prompt('What is your birth year? ');
+    const currentYear = 2022;
+    const age = currentYear - birthYear;
+    const greeting = alert(`Hello, ${name}! You are ${age} years old.`);
 }
 
 // Syntax for calling a function on button click
