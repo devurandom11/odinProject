29d08279a8163569fcb075b9c5847fc997fddb75
Mike Crawford <dev.random.87@gmail.com>
2022-06-30 10:38:16 -0700

    Refactor to include button and propper logic

--- Foundations/javascript-1/loginPrompt.js
+++ Foundations/javascript-1/loginPrompt.js
@@ -1,24 +1,27 @@
 // Script to authenticate user with a known password
-
+let button = document.querySelector('button');
 const PASSWORD = 'TheMaster'
 
-let user = prompt("Enter your username: ")
-
-if (user === "Admin") {
-    let password = prompt("Enter your password: ");
-    if (password === "TheMaster") {
-        alert("Welcome!");
+function authenticateUser() {
+    let user = prompt("Enter your username: ")
+    if (user === "Admin") {
+        let password = prompt("Enter your password: ");
+        if (password === "TheMaster") {
+            alert("Welcome!");
+        }
+        else if ( (password == null) || (password.length === 0)) {
+            alert("Canceled");
+        }
+        else {
+            alert("Wrong Password");
+        }
     }
-    else if (password == ("" || null)) {
-        alert("Cancled");
+    else if ((user == null) || (user.length == 0)) {
+        alert("Canceled");
     }
     else {
-        alert("Wrong Password");
+        alert("I don't know you...");
     }
 }
-else if (user == ("" || null)) {
-    alert("Cancled");
-}
-else {
-    alert("I don't know you...");
-}
+
+button.addEventListener("click", authenticateUser)
