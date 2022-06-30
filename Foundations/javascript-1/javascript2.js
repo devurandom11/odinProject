2a0f905531b3e03a1a326de6fa7d08901ac2166f
Mike Crawford <dev.random.87@gmail.com>
2022-06-30 07:30:37 -0700

    Replace [] with charAt()

--- Foundations/javascript-1/javascript2.js
+++ Foundations/javascript-1/javascript2.js
@@ -9,12 +9,12 @@ function multiply(num1, num2) {
 
 // Write a function that takes a string and returns the string with ONLY the first letter capitalized and the rest lowercase.
 function capitalize(string) {
-    return (string[0].toUpperCase() + (string.substring(1, ).toLowerCase()));
+    return (string[0].toUpperCase() + (string.substring(1).toLowerCase()));
 }
 
 // Write a function that takes in a string and returns the last character of that string.
 function lastLetter(string) {
-    return string[string.length - 1];
+    return string.charAt(string.length - 1);
 }
 
 // Test all functions above.
