badee9b234e9fea1e54028886edb0595e6ba26db
Mike Crawford <dev.random.87@gmail.com>
2022-06-30 10:08:31 -0700

    Add checkRange function and tests

--- Foundations/javascript-1/javascript3.js
+++ Foundations/javascript-1/javascript3.js
@@ -11,3 +11,23 @@ function greet() {
 // Syntax for calling a function on button click
 
 button.addEventListener('click', greet);
+
+
+// More Exercises
+
+function checkRange(num) {
+    if (num >= 14 && num <= 90) {
+        console.log("Congratulations, you may enter!");
+    }
+    else if (num < 14) {
+        console.log("Sorry, you are too young to enter :(");
+    }
+    else if (num > 90) {
+        console.log("Sorry, you are too old to enter :(");
+    }
+}
+// Test cases
+checkRange(2);
+checkRange(14);
+checkRange(90);
+checkRange(91);
