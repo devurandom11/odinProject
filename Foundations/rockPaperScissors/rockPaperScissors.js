c8b4a608715e45d26247b9dab5528a81e9d0a451
Mike Crawford <dev.random.87@gmail.com>
2022-07-01 08:25:38 -0700

    Write pseudocode for game

---
+++ Foundations/rockPaperScissors/rockPaperScissors.js
@@ -0,0 +1,15 @@
+// Design a game of Rock, Paper, Scissors between a player and the computer. The game should be played until one player reaches 5 points.
+
+// Set scores to 0
+
+// Play game function
+
+    // Get computer random choice
+
+    // Get player choice
+
+    // Compare player choice to computer choice and select winner
+
+    // Add point to winner
+
+    // Repeat play game function until one score === 5
