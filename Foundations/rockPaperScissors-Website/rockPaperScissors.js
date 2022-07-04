befd5e9ddf7fed9b9ddb8afd46231e3421802368
Mike Crawford <dev.random.87@gmail.com>
2022-07-04 00:50:44 -0400

    Add logic for displaying player choice

--- Foundations/rockPaperScissors-Website/rockPaperScissors.js
+++ Foundations/rockPaperScissors-Website/rockPaperScissors.js
@@ -18,14 +18,21 @@ function playGame() {
 function playRound() {
   let playerChoice;
   let computerChoice;
+  // Hide play game button
+  document.getElementById("play-game-btn").style.display='none';
+  // Display player choice buttons
+  document.getElementbyID("rock-button").style.display="inline-block";
+  document.getElementbyID("paper-button").style.display="inline-block";
+  document.getElementbyID("scissors-button").style.display="inline-block";
+
   // Player
-  if (document.getElementById("rock-button").clicked) {
-    playerChoice = "rock";
-  } else if (document.getElementById("paper-button").clicked) {
-    playerChoice = "paper";
-  } else {
-    playerChoice = "scissors";
-  }
+  // if (document.getElementById("rock-button").clicked) {
+  //   playerChoice = "rock";
+  // } else if (document.getElementById("paper-button").clicked) {
+  //   playerChoice = "paper";
+  // } else {
+  //   playerChoice = "scissors";
+  // }
   // Computer
   computerChoice = computerChoices[Math.floor(Math.random() * 3)];
   alert(
