6deb46f7d06fe582b8fa8cb283074981f1bc65e5
Mike Crawford <dev.random.87@gmail.com>
2022-07-06 20:30:26 -0700

    Declare constant variables for DOM

---
+++ Foundations/javascript-2/main.js
@@ -0,0 +1,7 @@
+// Assign desired elements to variables, starting with identifying parent div
+const container = document.querySelector("#container");
+const pContent = document.createElement('p');
+const blueHeader = document.createElement('h3');
+const innerDiv = document.createElement('div');
+const divHeader = document.createElement('h1');
+const divP = document.createElement('p');
